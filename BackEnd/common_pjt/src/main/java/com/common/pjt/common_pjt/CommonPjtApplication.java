c95cc73e55386e7aa3433d6873885826a19cf2ff
백승진 <mixkio@naver.com>
2022-07-21 10:13:35 +0900

    SpringBoot Project 생성

---
+++ BackEnd/common_pjt/src/main/java/com/common/pjt/common_pjt/CommonPjtApplication.java
@@ -0,0 +1,13 @@
+package com.common.pjt.common_pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonPjtApplication.class, args);
+	}
+
+}
---
+++ BackEnd/common_pjt/src/test/java/com/common/pjt/common_pjt/CommonPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.pjt.common_pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
