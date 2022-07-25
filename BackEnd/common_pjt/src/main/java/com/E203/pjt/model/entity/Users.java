372ed08da33974ad606ebd84f2aff8c132da1d33
백승진 <mixkio@naver.com>
2022-07-25 14:11:14 +0900

    [feat] Parties Entity upload

--- BackEnd/common_pjt/src/main/java/com/E203/pjt/model/entity/Users.java
+++ BackEnd/common_pjt/src/main/java/com/E203/pjt/model/entity/Users.java
@@ -2,6 +2,8 @@ package com.E203.pjt.model.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -18,33 +20,34 @@ import lombok.Setter;
 @Table(name = "users")
 public class Users {
   @Id
-  @Column(name = "user_seq")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_seq", nullable = false, columnDefinition = "INT UNSIGNED UNIQUE")
   private Integer userSeq;
 
-  @Column(name = "user_email", length = 45)
+  @Column(name = "user_email", nullable = false, length = 45)
   private String userEmail;
 
-  @Column(name = "user_phone", length = 20)
+  @Column(name = "user_phone", nullable = false, length = 20)
   private String userPhone;
 
-  @Column(name = "user_nickname", length = 20)
+  @Column(name = "user_nickname", nullable = false, length = 20)
   private String userNickname;
 
-  @Column(name = "user_password", length = 20)
+  @Column(name = "user_password", nullable = false, length = 20)
   private String userPassword;
 
-  @Column(name = "user_image", length = 45)
+  @Column(name = "user_image", nullable = false, length = 45)
   private String userImage;
 
-  @Column(name = "user_rating")
+  @Column(name = "user_rating", nullable = false)
   private Integer userRating;
 
-  @Column(name = "user_lat", length = 20)
+  @Column(name = "user_lat", nullable = false, length = 20)
   private String userLat;
 
-  @Column(name = "user_lng", length = 20)
+  @Column(name = "user_lng", nullable = false, length = 20)
   private String userLng;
 
-  @Column(name = "user_account", length = 45)
+  @Column(name = "user_account", nullable = false, length = 45)
   private String userAccount;
 }
