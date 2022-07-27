bfa1ca044eb343b295dc71b33e867c7a57e5c07a
김유리 <goldflowed@gmail.com>
2022-07-27 10:17:09 +0900

    [fix] 복수형을 단수형으로 변경

--- BackEnd/common_pjt/src/main/java/com/E203/pjt/repository/ChatRoomsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.E203.pjt.repository;
-
-import com.E203.pjt.model.entity.ChatRooms;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRoomsRepository extends JpaRepository<ChatRooms, Integer> {
-}
