7b511e186ee28d035cea0e91c5b14c449e3969fd
김유리 <goldflowed@gmail.com>
2022-07-28 10:20:39 +0900

    [feat] querydsl set

--- BackEnd/common_pjt/src/main/java/com/E203/pjt/repository/PartyRepositorySupport.java
+++ BackEnd/common_pjt/src/main/java/com/E203/pjt/repository/PartyRepositorySupport.java
@@ -1,6 +1,7 @@
 package com.E203.pjt.repository;
 
 import com.E203.pjt.model.entity.Party;
+import com.E203.pjt.model.entity.QParty;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,9 +13,13 @@ public class PartyRepositorySupport {
     @Autowired
     private JPAQueryFactory jpaQueryFactory;
 
+    QParty party = QParty.party;
 
-    public List<Party> findDynamicQueryMyPartyList(){
-
-        return null;
-    }
+//    public List<Party> findDynamicQueryMyPartyList(Integer userSeq){
+//        List<Party> result = jpaQueryFactory
+//                .select(Party.class)
+//                .from(party)
+//                .fetch();
+//        return null;
+//    }
 }
