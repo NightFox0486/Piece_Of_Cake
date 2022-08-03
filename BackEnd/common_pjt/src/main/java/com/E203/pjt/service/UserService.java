8bef709560658a25fc1bc19c3e98c4ff2f5434a8
김유리 <goldflowed@gmail.com>
2022-08-03 22:34:33 +0900

    [feat] add User & Party VO, modify PartyController method return value

--- BackEnd/common_pjt/src/main/java/com/E203/pjt/service/UserService.java
+++ BackEnd/common_pjt/src/main/java/com/E203/pjt/service/UserService.java
@@ -2,10 +2,12 @@ package com.E203.pjt.service;
 
 import java.util.List;
 
+import com.E203.pjt.model.dto.req.UserReqVO;
+import com.E203.pjt.model.dto.res.UserResVO;
 import com.E203.pjt.model.entity.User;
 
 public interface UserService {
-  User createUser(User user);
+  UserResVO createUser(UserReqVO userReqVO);
   void deleteUser(Integer user_seq);
   List<User> getAllUsers();
 }
