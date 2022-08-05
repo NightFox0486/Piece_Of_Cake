b5b9ff2631dc7fc3178bf3d53c09486e822303bb
김유리 <goldflowed@gmail.com>
2022-08-05 22:53:53 +0900

    [feat] change work project & add user info

--- FrontEnd/KYR/poc/test/widget_test.dart
+++ FrontEnd/KYR/poc/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:poc/main.dart';
+import 'package:piece_of_cake/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
