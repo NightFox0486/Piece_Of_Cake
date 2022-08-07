86f50d8d5981c73ee62a24b577590013fc344b21
김유리 <goldflowed@gmail.com>
2022-08-08 07:56:29 +0900

    [chore] for sync

---
+++ FrontEnd/KYR/poc/lib/pie_create.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PieCreate extends StatelessWidget {
+  const PieCreate({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('BuyCreate')
+      ),
+    );
+  }
+}
