f952760d5c72123b45fa55998f5dfd73e30fd1d9
김유리 <goldflowed@gmail.com>
2022-08-07 14:57:21 +0900

    [feat] my page test

--- FrontEnd/KYR/poc/lib/main.dart
+++ FrontEnd/KYR/poc/lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:piece_of_cake/kakao/kakao_login_view_model.dart';
 
 import 'package:piece_of_cake/kakao/kakao_login.dart';
+import 'package:piece_of_cake/wish.dart';
+
+import 'chat.dart';
+import 'home.dart';
+import 'login.dart';
+import 'my.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: '2157d1da3704b84b219793633746ca5c');
   runApp(const MyApp());
+  // runApp(
+  //     MaterialApp(
+  //       initialRoute: '/',
+  //       routes: {
+  //         '/': (context) => const Home(),
+  //         '/login': (context) => const LoginPage(),
+  //         '/wish': (context) => const Wish(),
+  //         '/chat': (context) => const Chat(),
+  //         '/my': (context) => const My(),
+  //       },
+  //     )
+  // );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,103 +34,96 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Piece Of Cake',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainPageState extends State<MainPage> {
+  // kakao login user
   final viewModel = KakaoLoginViewModel(KakaoLogin());
 
+  // navigation bar
+  final navigationKey = GlobalKey<CurvedNavigationBarState>();
+  // final screens = [
+  //   '/',
+  //   'wish',
+  //   'chat',
+  //   'my',
+  // ];
+  final screens = [
+    Home(),
+    Wish(),
+    Chat(),
+    My(),
+  ];
+  int index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Image.network(viewModel.user?.kakaoAccount?.profile?.profileImageUrl ?? ''),
-            Text(
-              '${viewModel.isLogined}',
-            ),
-            ElevatedButton(
-                onPressed: () async{
-                  await viewModel.login();
-                  setState(() {
-
-                  }); // setState(() {}); <- 아무것도 안하고 화면 갱신
-                },
-                child: Text('Login')
-            ),
-            ElevatedButton(
-                onPressed: () async{
-                  await viewModel.logout();
-                  setState(() {
-
-                  });
-                },
-                child: Text('Logout')
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                User user = await viewModel.me();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyPage(user)),
-                );
-              },
-              child: Text('My Page'),
-            ),
-          ],
+    final items = <Widget>[
+      Icon(Icons.home, size:30),
+      Icon(Icons.celebration, size:30),
+      // Icon(Icons.add, size:30),
+      Icon(Icons.question_answer, size:30),
+      Icon(Icons.person, size:30),
+    ];
+    if (viewModel.isLogined) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Piece Of Cake'),
         ),
-      ),
-    );
+        body: screens[index],
+        bottomNavigationBar: CurvedNavigationBar(
+          items: items,
+          key: navigationKey,
+          backgroundColor: Colors.white,
+          color: Colors.pink,
+          onTap: (index) {
+            setState(() {
+              this.index = index;
+            });
+          },
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('로그인 해야함'),
+        ),
+        body: LoginPage(),
+      );
+    }
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('Piece Of Cake'),
+    //   ),
+    //   body: screens[index],
+    //   bottomNavigationBar: CurvedNavigationBar(
+    //     items: items,
+    //     key: navigationKey,
+    //     backgroundColor: Colors.white,
+    //     color: Colors.pink,
+    //     onTap: (index) {
+    //       setState(() {
+    //         this.index = index;
+    //       });
+    //     },
+    //   ),
+    // );
   }
 }
 
-class MyPage extends StatefulWidget {
-  // MyPage(User user, {Key? key}) : super(key: key);
-  late User _user;
-  MyPage(User user) {
-    this._user = user;
-  }
-
-  @override
-  State<MyPage> createState() => _MyPageState(_user);
-}
-
-class _MyPageState extends State<MyPage> {
-  late User _user;
-  _MyPageState(User user) {
-    this._user = user;
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text(_user.id.toString()),
-          Text(_user.kakaoAccount?.email ?? ''),
-          Text(_user.kakaoAccount?.profile?.nickname ?? ''),
-        ],
-      ),
-    );
-  }
-}
