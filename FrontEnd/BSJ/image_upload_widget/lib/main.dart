e57cb0b41e2c29d52108b54a0ac8d52c3299b2a2
백승진 <mixkio@naver.com>
2022-08-10 16:17:04 +0900

    [fix] image add backup

--- FrontEnd/BSJ/image_upload_widget/lib/main.dart
+++ FrontEnd/BSJ/image_upload_widget/lib/main.dart
@@ -1,4 +1,4 @@
-//import 'dart:html';
+// import 'dart:html';
 import 'dart:io' as io;
 
 import 'package:firebase_core/firebase_core.dart';
@@ -25,79 +25,130 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static List<Widget> images = [
-    Container(
-      child: Image.asset('img/thumb.jpg',
-          fit: BoxFit.fill, width: 100, height: 100),
-    )
-  ];
+  final ImagePicker imagePicker = ImagePicker();
 
-  Future<String>? uploadFile(XFile? file) async {
-    if (file == null) {
-      print("input images is null");
-      return null!;
+  List<XFile>? imageFileList = [];
+
+  void selectImages() async {
+    final List<XFile>? selectedImages = await imagePicker.pickMultiImage();
+    if (selectedImages!.isNotEmpty) {
+      imageFileList!.addAll(selectedImages);
     }
-
-    Reference ref = FirebaseStorage.instance
-        .ref()
-        .child('image-test')
-        .child('/test-image.jpg');
-    final metadata = SettableMetadata(
-      contentType: 'image/jpeg',
-      customMetadata: {'picked-file-path': file.path},
-    );
-    if (kIsWeb) {
-      ref.putData(await file.readAsBytes(), metadata);
-    } else {
-      ref.putFile(io.File(file.path), metadata);
-    }
-    String url = await ref.getDownloadURL();
-    return url;
-  }
-
-  addImgStorage() async {
-    final file = await ImagePicker().pickImage(source: ImageSource.gallery);
-    String? url = await uploadFile(file);
-    if (url != null) {
-      images.insert(
-          0,
-          Container(
-            child: Image.network(url, width: 100, height: 100),
-          ));
-    }
-    setState(() {});
-  }
-
-  addImg() {
     setState(() {
-      addImgStorage();
     });
   }
-
-  removeImg() {
-    setState(() {
-      images.removeLast();
-    });
-  }
+  // static List<Widget> images = [
+  //   // Container(
+  //   //   child: Image.asset('img/thumb.jpg',
+  //   //       fit: BoxFit.fill, width: 100, height: 100),
+  //   // )
+  // ];
+  //
+  // Future<String>? uploadFile(XFile? file,int index) async {
+  //   if (file == null) {
+  //     print("input images is null");
+  //     return null!;
+  //   }
+  //
+  //   Reference ref = FirebaseStorage.instance
+  //       .ref()
+  //       .child('image-test')
+  //       .child('/test-image$index.jpg');
+  //   final metadata = SettableMetadata(
+  //     contentType: 'image/jpeg',
+  //     customMetadata: {'picked-file-path': file.path},
+  //   );
+  //   if (kIsWeb) {
+  //     ref.putData(await file.readAsBytes(), metadata);
+  //   } else {
+  //     ref.putFile(io.File(file.path), metadata);
+  //   }
+  //   String url = await ref.getDownloadURL();
+  //   return url;
+  // }
+  //
+  // addImgStorage(int index) async {
+  //   final file = await ImagePicker().pickImage(source: ImageSource.gallery);
+  //   String? url = await uploadFile(file, index);
+  //   if (url != null) {
+  //     images.insert(
+  //         0,
+  //         Container(
+  //           child: Image.network(url, width: 100, height: 100),
+  //         ));
+  //   }
+  //   setState(() {});
+  // }
+  //
+  // addImg() {
+  //
+  //     addImgStorage(0);
+  // }
+  //
+  // removeImg() {
+  //   setState(() {
+  //     images.removeLast();
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Row(
-          textDirection: TextDirection.ltr,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ...images,
-            Column(children: [
-              TextButton.icon(
-                  onPressed: addImg, icon: Icon(Icons.add), label: Text('add')),
-              TextButton.icon(
-                  onPressed: removeImg,
-                  icon: Icon(Icons.remove),
-                  label: Text('remove'))
-            ])
-          ]),
+    // return MaterialApp(
+    //   home: Row(
+    //       textDirection: TextDirection.ltr,
+    //       mainAxisAlignment: MainAxisAlignment.start,
+    //       crossAxisAlignment: CrossAxisAlignment.start,
+    //       children: [
+    //         ...images,
+    //         Column(children: [
+    //           TextButton.icon(
+    //               onPressed: addImg, icon: Icon(Icons.add), label: Text('add')),
+    //           TextButton.icon(
+    //               onPressed: removeImg,
+    //               icon: Icon(Icons.remove),
+    //               label: Text('remove'))
+    //         ])
+    //       ]),
+    // );
+    return MaterialApp(home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Image Upload Widget"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+
+              SizedBox(height: 20,),
+              Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GridView.builder(
+                        itemCount: imageFileList!.length+1,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          return Image.file(io.File(imageFileList![index].path), fit: BoxFit.cover);
+                        },
+                    ),
+                  )
+              ),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text(
+                      "Pick Images from Gallery",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold
+                      )
+                  ),
+                  onPressed: () {
+                    selectImages();
+                  }
+              ),
+            ],
+          ),
+        )
+    )
     );
   }
   // TextButton.icon(
