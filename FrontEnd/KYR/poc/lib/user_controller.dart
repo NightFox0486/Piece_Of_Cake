7fc669d1b50df6979400a2af74d2632cde1898dc
김유리 <goldflowed@gmail.com>
2022-08-10 09:26:37 +0900

    [feat] delete previous project

--- FrontEnd/KYR/poc/lib/user_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:piece_of_cake/kakao/kakao_login_view_model.dart';
-
-import 'kakao/kakao_login.dart';
-
-class UserController extends GetxController {
-}
