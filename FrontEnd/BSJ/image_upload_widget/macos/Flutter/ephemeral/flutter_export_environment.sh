d471bddc0f8b53c5da1ba1074e20c5c074a9682e
백승진 <mixkio@naver.com>
2022-08-11 17:33:20 +0900

    [backup] 2022.08.11 daily back up

---
+++ FrontEnd/BSJ/image_upload_widget/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SSAFY\Desktop\E203_common_pjt\e203_common_pjt\FrontEnd\BSJ\image_upload_widget"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
