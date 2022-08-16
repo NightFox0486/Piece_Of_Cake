5fec9af10778f9696916a588c6ffbb6754f43fde
백승진 <mixkio@naver.com>
2022-08-16 11:50:45 +0900

    Update BackEnd/common_pjt/src/main/java/com/E203/pjt/controller/PartyController.java

--- BackEnd/common_pjt/src/main/java/com/E203/pjt/controller/PartyController.java
+++ BackEnd/common_pjt/src/main/java/com/E203/pjt/controller/PartyController.java
@@ -78,7 +78,7 @@ public class PartyController {
     return partyHostList;
   }
 
-  @PutMapping(value = "/party/{partySeq}")
+  @PutMapping(value = "/party")
   public void addPartyMainImage(@RequestParam Integer partySeq, String partyMainImageUrl){
     System.out.println(partySeq);
     System.out.println(partyMainImageUrl);
